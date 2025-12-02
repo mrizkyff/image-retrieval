9cb73fdc78eab39b58d19c651cf7d352af367676
mrizkyff <muhamadrizkyff@gmail.com>
2025-12-02 14:48:55 +0700

    .env

--- node-service/config/config.js
+++ node-service/config/config.js
@@ -2,19 +2,19 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.POSTGRES_USER || 'admin',
-    password: process.env.POSTGRES_PASSWORD || 'admin',
-    database: process.env.POSTGRES_DB || 'postgres',
-    host: process.env.POSTGRES_HOST || 'localhost',
-    port: Number(process.env.POSTGRES_PORT || 5432),
+    username: 'admin',
+    password: 'admin',
+    database: 'postgres',
+    host: '172.17.0.1',
+    port: 5432,
     dialect: 'postgres'
   },
   production: {
-    username: process.env.POSTGRES_USER || 'admin',
-    password: process.env.POSTGRES_PASSWORD || 'admin',
-    database: process.env.POSTGRES_DB || 'postgres',
-    host: process.env.POSTGRES_HOST || 'localhost',
-    port: Number(process.env.POSTGRES_PORT || 5432),
+    username: 'admin',
+    password: 'admin',
+    database: 'postgres',
+    host: '172.17.0.1',
+    port: 5432,
     dialect: 'postgres'
   }
 };
